8415b55ab0272de1fdcb8d0c38503456c5038109
Harbans Lal <84002120+Harbans-Lal@users.noreply.github.com>
2024-09-05 18:49:31 +0530

    CASL basic practice in nextjs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return( 
   
       <FluentProvider theme={webLightTheme} >
-          <AbilityContext.Provider value={{ability}}> 
+          <AbilityContext.Provider value={ability}> 
              <Component {...pageProps} />
           </AbilityContext.Provider>
       </FluentProvider>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,21 +6,23 @@ import { Can } from '@/casl/abilityFor';
 import defineAbilitiesFor from '@/casl/abilityFor';
 
 const Home = () => {
-  const {ability} = useContext(AbilityContext);
+  // const {ability} = useContext(AbilityContext);
   const [isAbleToRead, setIsAbleToRead] = useState(false)
   const [isAbleToDelete, setIsAbleToDelete] = useState(false);
   useEffect(() =>{
-    let whoAmI = 'admin';
+    const whoAmI:string = 'user';
     const role = defineAbilitiesFor(whoAmI)
     setIsAbleToRead(role.can('read','post'));
     setIsAbleToDelete(role.can('delete','post'));
 
-  })
+  },[])
  return(
     <div>
      {isAbleToRead && <Display>You can read it </Display>}
      {isAbleToDelete && <Button>You can delete</Button>}
-      
+      <Can not I="read" this="post">
+        <h1>YOu are not allowed to see the content !!</h1>
+      </Can>
     </div>
  );
 };
